7372345e618c53d4c4b6d270130f8009b85d517a
Sergey Lebedinets <sergey.lebedinets@headworks.com.ua>
2020-08-29 16:27:35 +0300

    1. provide sign out feature 2. polish style

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle} from "../../firebase/firebase.utils";
+import {signInWithGoogle} from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -52,8 +52,10 @@ class SignIn extends React.Component {
             label="Password"
             type="password"
           />
-          <CustomButton type="submit">Sign in </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+          </div>
         </form>
       </div>
     )
